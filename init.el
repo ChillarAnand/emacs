065f3bea2b48dc68cc5cf9bed7bc09fce641ea45
K3 <anand.r@leucinerichbio.com>
2014-12-19 12:49:54 +0530

    added emacs-for-python package

--- init.el
+++ init.el
@@ -177,7 +177,21 @@
 
 ;; clean whitespace before saving buffer
 (add-hook 'before-save-hook 'whitespace-cleanup)
-;; Local Variables:
-;; coding: utf-8
-;; no-byte-compile: t
-;; End:
+
+
+;; emacs for python
+(load-file "~/.emacs.d/emacs-for-python/epy-init.el")
+
+(add-to-list 'load-path "~/.emacs.d/emacs-for-python/") ;; load the various files
+(require 'epy-setup)      ;; It will setup other loads, it is required!
+(require 'epy-python)     ;; If you want the python facilities [optional]
+;;(require 'epy-completion) ;; If you want the autocompletion settings [optional]
+(require 'epy-editing)    ;; For configurations related to editing [optional]
+(require 'epy-bindings)   ;; For my suggested keybindings [optional]
+(require 'epy-nose)       ;; For nose integration
+
+(epy-setup-ipython)
+
+;; highlight line color.
+;; (global-hl-line-mode t)
+;; (set-face-background 'hl-line "grey")
